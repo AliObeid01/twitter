0cf0a97bf9f735391ba7147bec929f9cc46ab445
unknown <aliiobeid01@gmail.com>
2022-09-18 22:04:24 +0300

    create the api to count the following users

---
+++ backend/following.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Access-Control-Allow-Origin: http://localhost/');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header("Content-type:application/json");
+
+include("connection.php");
+
+$id=$_POST['id'];
+$query = $mysqli->prepare("SELECT id,fullname,username FROM users WHERE EXISTS (SELECT * from relations where relations.user_id = $id AND relations.follow_id= users.id)");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($info = $array->fetch_assoc()){
+    $response[] = $info;
+}
+
+$json = json_encode($response);
+echo $json;
+
+?>
